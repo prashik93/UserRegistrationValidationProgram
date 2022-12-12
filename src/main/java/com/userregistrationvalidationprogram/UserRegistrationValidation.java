0f38faa20978563f53ed839e16ba1dcdb1e55e97
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-12 21:17:45 +0530

    ADDED : Code to validate Last Name.

--- src/main/java/com/userregistrationvalidationprogram/UserRegistrationValidation.java
+++ src/main/java/com/userregistrationvalidationprogram/UserRegistrationValidation.java
@@ -16,4 +16,15 @@ public class UserRegistrationValidation {
         return "Invalid";
     }
 
+    public String validateLastName(String lastName) {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.find();
+        if(result) {
+            System.out.println("Last Name is Valid");
+            return "Valid";
+        }
+        System.out.println("Invalid, Last Name should Start with UpperCase letter and has minimum 3 characters");
+        return "Invalid";
+    }
 }
--- src/test/java/com/userregistrationvalidationprogramtest/UserRegistrationValidationTest.java
+++ src/test/java/com/userregistrationvalidationprogramtest/UserRegistrationValidationTest.java
@@ -5,11 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UserRegistrationValidationTest {
+    UserRegistrationValidation userRegistrationValidation = new UserRegistrationValidation();
     @Test
     public void whenGivenFirstName_shouldCheckValidOrInvalid() {
-        UserRegistrationValidation userRegistrationValidation = new UserRegistrationValidation();
         String usrFirstName = "Prashik";
         String result = userRegistrationValidation.validateFirstName(usrFirstName);
+        assertEquals("Valid", result);
+    }
+
+    @Test
+    public void whenGivenLastName_shouldCheckValidOrInvalid() {
+        String usrLastName = "Kamble";
+        String result = userRegistrationValidation.validateLastName(usrLastName);
         assertEquals("Valid", result);
     }
 }
